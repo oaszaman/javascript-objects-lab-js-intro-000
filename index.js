5f66df2905e0b757af41ead350f29e6285055d34
Shahzab Zaman <o.aszaman00@gmail.com>
2021-03-28 19:02:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ object[key] = value;
 return object;
 }
 
-function deleteFromObjectsByKey(object, key) {
+function deleteFromObjectByKey(object, key) {
 var newObj = Object.assign({}, object);
 delete newObj[key];
 return newObj;
