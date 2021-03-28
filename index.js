1df41b58c5edafcd9e94481de9bb23d8ce7cccfc
Shahzab Zaman <o.aszaman00@gmail.com>
2021-03-28 18:57:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-
+return Object.assign({}, object, {[key]:value});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-
+object[key] = value;
+return object;
 }
 
 function deleteFromObjectsByKey(object, key) {
-
+var newObj = Object.assign({}, object);
+delete newObj[key];
+return newObj;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-
+delete object[key];
+return object;
 }
